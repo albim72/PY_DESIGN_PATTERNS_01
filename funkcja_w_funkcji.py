f9b27da8f73e44861f3d9116d0e98e97e6d8d2be
albim72 <marcin.albiniak72@gmail.com>
2023-09-09 10:47:56 +0200

    Update funkcja_w_funkcji.py

--- funkcja_w_funkcji.py
+++ funkcja_w_funkcji.py
@@ -1,25 +1,29 @@
-#przykład 1
+# przykład 1
 
 def witaj(imie):
     return f'dziękujemy za złożenie konta: {imie}'
 
-def egzamin(imie,punkty,zaliczono):
+
+def egzamin(imie, punkty, zaliczono):
     return f'osoba egzaminowana: {imie}, liczba punktów: {punkty}, egzamin zdany: {zaliczono}'
 
-def osoba(funkcja,*args):
+
+def osoba(funkcja, *args):
     return funkcja(*args)
 
-print(osoba(witaj,"Leon"))
-print(osoba(egzamin,"Olga",57,"tak"))
 
-liczba = [3,6,78,9,12,11,34,33,101,2,0,-5,-33,-2,19]
+print(osoba(witaj, "Leon"))
+print(osoba(egzamin, "Olga", 57, "tak"))
 
-parz = list(filter(lambda x:x%2==0,liczba))
+liczba = [3, 6, 78, 9, 12, 11, 34, 33, 101, 2, 0, -5, -33, -2, 19]
+
+parz = list(filter(lambda x: x % 2 == 0, liczba))
 print(parz)
-cube = list(map(lambda x:x**3,liczba))
+cube = list(map(lambda x: x ** 3, liczba))
 print(cube)
 
-#przykład 2
+
+# przykład 2
 
 def rejestracja(oplata):
     def lista_zawodnikow(nrlisty):
@@ -38,6 +42,30 @@ def rejestracja(oplata):
     else:
         return error
 
+
 print(rejestracja(1)(456))
 print(rejestracja(33)())
 print(rejestracja(0)())
+
+
+# przykład 3   prosty dekorator
+def startstop(funkcja):
+    def wrapper(*args):
+        print("uruchomomienie nowej funkcji...")
+        funkcja(*args)
+        print("działanie funkcji zakończone!")
+
+    return wrapper
+
+def zawijanie(czego):
+    print(f'zawijanie {czego} w sreberka')
+
+zw = startstop(zawijanie)
+zw("czekoladek")
+zawijanie("cukierków")
+
+@startstop
+def dmuchanie(czego):
+    print(f'dmuchanie {czego} na torcie w urodziny')
+
+dmuchanie("świeczek")
