f4e5c8b0cf87c7e78ffb30e2015dcb7b3100581a
albim72 <marcin.albiniak72@gmail.com>
2023-09-09 11:37:53 +0200

    Update dekoratory.py

--- dekoratory.py
+++ dekoratory.py
@@ -14,8 +14,39 @@ def pomiarczasu(funkcja):
         print(wynik[0])
     return wrapper
 
+def sleepdec(funkcja):
+    def wrapper(*args):
+        time.sleep(3)
+        return funkcja(*args)
+    return wrapper
+
+
 @pomiarczasu
+@sleepdec
 def big_nb():
     sum([i**2 for i in range(10_000_000)])
 
 big_nb()
+
+@sleepdec
+def info(k):
+    return 9*k
+
+print(info(567))
+
+#przykład 2
+def debug(funkcja):
+    def wrapper(*args,**kwargs):
+        print(f'wołana funkcja to: {funkcja.__name__}')
+        funkcja(*args)
+    return wrapper
+
+@debug
+def info(i):
+    print(f'ważna informacja: {i}')
+
+info(f"kod nr: 6678")
+
+
+
+
