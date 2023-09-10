4e704d77f09c700eb81327aaf3a97bd6fcb8971b
albim72 <marcin.albiniak72@gmail.com>
2023-09-10 16:26:21 +0200

    Update main.py

--- DESIGN PATTERNS/ITERATOR/main.py
+++ DESIGN PATTERNS/ITERATOR/main.py
@@ -2,10 +2,10 @@ class FootballTeamIterator:
     def __init__(self,members):
         self.memebers = members
         self.index = 0
-        
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
         if self.index < len(self.memebers):
             val = self.memebers[self.index]
@@ -13,3 +13,23 @@ class FootballTeamIterator:
             return val
         else:
             raise StopIteration()
+
+class FootballTeam:
+    def __init__(self,members):
+        self.members = members
+
+    def __iter__(self):
+        return FootballTeamIterator(self.members)
+
+
+def main():
+    members = [f'gracz{str(x)}' for x in range(1,23)]
+    members = members + ['trener1','trener2','trener3','fizjoterapeuta','rzecznik']
+    team = FootballTeam(members)
+    team_it = iter(team)
+
+    while True:
+        print(next(team_it))
+
+if __name__ == '__main__':
+    main()
