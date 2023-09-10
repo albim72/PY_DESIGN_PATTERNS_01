0be063558afb7e562fe1017267b1c81dfd61a733
albim72 <marcin.albiniak72@gmail.com>
2023-09-10 14:50:33 +0200

    Update main.py

--- DESIGN PATTERNS/interpreter/main.py
+++ DESIGN PATTERNS/interpreter/main.py
@@ -70,8 +70,24 @@ class Boiler:
 
     def increase_temperature(self,amount):
         print(f"increasing the boiler's temperature by {amount} degress")
-        self.itemperature += amount
+        self.temperature += amount
 
     def decrease_temperature(self, amount):
         print(f"decreasing the boiler's temperature by {amount} degress")
-        self.itemperature -= amount
+        self.temperature -= amount
+        
+class Fridge:
+    def __init__(self):
+        self.temperature = 2
+
+    def __str__(self):
+        return f'fridge temperature: {self.temperature} C'
+
+    def increase_temperature(self,amount):
+        print(f"increasing the fridge's temperature by {amount} degress")
+        self.temperature += amount
+
+    def decrease_temperature(self, amount):
+        print(f"decreasing the fridge's temperature by {amount} degress")
+        self.temperature -= amount
+        
