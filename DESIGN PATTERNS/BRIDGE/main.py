fe9d6d20523750b5ddf17c2c10fecdb10ebf667d
albim72 <marcin.albiniak72@gmail.com>
2023-09-10 15:55:20 +0200

    Update main.py

--- DESIGN PATTERNS/BRIDGE/main.py
+++ DESIGN PATTERNS/BRIDGE/main.py
@@ -5,16 +5,16 @@ import urllib.request
 class ResourceContent:
     def __init__(self,imp):
         self._imp = imp
-        
+
     def show_content(self,path):
         self._imp.fetch(path)
-        
+
 class ResourceContentFetcher(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def fetch(self,path):
         pass
-    
-    
+
+
 class URLFetcher(ResourceContentFetcher):
     def fetch(self,path):
         req = urllib.request.Request(path)
@@ -22,9 +22,23 @@ class URLFetcher(ResourceContentFetcher):
             if response.code == 200:
                 this_page = response.read()
                 print(this_page)
-                
+
 class LocalFileFetcher(ResourceContentFetcher):
     def fetch(self, path):
-        with open(path) as f:
+        with open(path, encoding='utf-8') as f:
             print(f.read())
-            
+
+def main():
+    url_fetcher = URLFetcher()
+    iface = ResourceContent(url_fetcher)
+    iface.show_content('http://python.org')
+
+    print("_"*30)
+
+    locals_fetcher = LocalFileFetcher()
+    iface = ResourceContent(locals_fetcher)
+    iface.show_content('info.txt')
+
+
+if __name__ == '__main__':
+    main()
