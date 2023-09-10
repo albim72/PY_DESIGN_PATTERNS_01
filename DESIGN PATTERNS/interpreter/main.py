cb2c6933117ffacfaf00481ca5d83af624608561
albim72 <marcin.albiniak72@gmail.com>
2023-09-10 14:43:31 +0200

    Update main.py

--- DESIGN PATTERNS/interpreter/main.py
+++ DESIGN PATTERNS/interpreter/main.py
@@ -14,7 +14,7 @@ class Gate:
     def close(self):
         print('closing the gate')
         self.is_open = False
-        
+
 class Garage:
     def __init__(self):
         self.is_open = False
@@ -29,3 +29,18 @@ class Garage:
     def close(self):
         print('closing the garage')
         self.is_open = False
+        
+class Aircondition:
+    def __init__(self):
+        self.is_on = False
+
+    def __str__(self):
+        return 'on' if self.is_on else 'off'
+    
+    def turn_on(self):
+        print('turning on the air condition')
+        self.is_on = True
+        
+    def turn_off(self):
+        print('turning off the air condition')
+        self.is_on = False
