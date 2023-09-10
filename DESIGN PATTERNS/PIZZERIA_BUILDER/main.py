b2bf21f85f84d635f8a34e8b6b1ad48f0dc0e470
albim72 <marcin.albiniak72@gmail.com>
2023-09-10 10:47:38 +0200

    Update main.py

--- DESIGN PATTERNS/PIZZERIA_BUILDER/main.py
+++ DESIGN PATTERNS/PIZZERIA_BUILDER/main.py
@@ -6,3 +6,20 @@ PizzaDough = Enum('PizzaDough','thin thick')
 PizzaSauce = Enum('PizzaSauce', 'tomato cream_fraiche')
 PizzaTopping = Enum('PizzaTopping','mozarella double_mozarella bacon ham mushrooms redonion oregano')
 STEP_DELAY = 3
+
+class Pizza:
+    def __init__(self,name):
+        self.name = name
+        self.dough = None
+        self.sauce = None
+        self.topping = []
+        
+    def __str__(self):
+        return self.name
+    
+    def prepare_dough(self,dough):
+        self.dough = dough
+        
+        print(f'prepairing the {self.dough.name} dough of your {self}')
+        time.sleep(STEP_DELAY)
+        print(f'done with the {self.dough.name} dough')
